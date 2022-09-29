3a23bbe5e0f222c4b85fae2cd1d1b425b8bdc6fb
breznovic <slymarkel@gmail.com>
2022-09-29 20:38:33 +0300

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './App.css'
-import Header from "./components/header/Header"
+import Header from './components/header/Header'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
     return <div className='App'>
-        <Header />
+        <Router>
+            <Header/>
+            <Routes>
+                <Route index element={<Home/>}></Route>
+                <Route path="movie/:id" element={<Movie/>}></Route>
+                <Route path="movies/:type" element={<MovieList/>}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </Router>
     </div>
 }
 
