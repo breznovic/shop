3952c4a74ceb4d84047ff0e0dc28a866809d1c27
breznovic <slymarkel@gmail.com>
2022-10-10 21:03:26 +0300

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css"
+import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import SinglePage from './components/watch/SinglePage'
 import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
 import HomePage from './pages/Home/HomePage'
+import Footer from './components/footer/Footer'
 
 function App() {
     return (
