06328c626d8cca2d3086262d9bf71a74733fb1ee
breznovic <slymarkel@gmail.com>
2022-10-15 23:20:06 +0300

    New

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import './App.css'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Header from './components/header/Header'
-import HomePage from './pages/Home/HomePage'
-import Footer from './components/footer/Footer'
-import SinglePage from "./components/watch/SinglePage"
 
 function App() {
     return (
         <>
-            <Router>
-                <Header/>
-                <Switch>
-                    <Route exact path='/' component={HomePage}/>
-                    <Route path='/singlepage/:id' component={SinglePage} exact/>
-                </Switch>
-                <Footer/>
-            </Router>
+          Test
         </>
     )
 }
